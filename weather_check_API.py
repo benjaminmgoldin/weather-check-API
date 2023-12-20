b91b6d16cb6828ac39d3feee1620139413aaa8f8
benjaminmgoldin <153044386+benjaminmgoldin@users.noreply.github.com>
2023-12-20 13:55:37 -0500

    weather_check_API

---
+++ weather_check_API.py
@@ -0,0 +1,20 @@
+import requests
+
+#Enter your API key below from openweathermap
+API_KEY = ""
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+city = input ("Please enter a city name:")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+#Code currently displays temperature in fahrenheit as the API call provides temperature in kelvin
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = (data["main"]["temp"] * 1.8 - 459.67)
+    
+    print("Weather:", weather)
+    print("Temperature:", temperature, "fahreinheit")    
+else:
+    print("This city is not recognized")
